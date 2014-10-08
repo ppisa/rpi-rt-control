76447f7db2f8dbeb94acd24761a6ab0b837b4353
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2014-10-08 17:19:37 +0200

    Include modprobe rpi_gpio_irc_module command into model startup script.
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- simulink/run_model.sh
+++ simulink/run_model.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xterm -e "scp -i target-ssh-key ./$1 root@$2:/tmp ; echo model $1 copied to target is run ; ssh  -i target-ssh-key root@$2 /tmp/$1 -tf inf -w ; sleep 2" &
+xterm -e "scp -i target-ssh-key ./$1 root@$2:/tmp ; echo model $1 copied to target is run ; ssh  -i target-ssh-key root@$2 modprobe rpi_gpio_irc_module ; ssh  -i target-ssh-key root@$2 /tmp/$1 -tf inf -w ; sleep 2" &
