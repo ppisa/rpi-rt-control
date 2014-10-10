41efd379d1c2364eac10e54f4709051916ce950c
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2014-10-10 18:05:35 +0200

    Run model updated to work with user global SSH key.
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- simulink/run_model.sh
+++ simulink/run_model.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-xterm -e "scp -i target-ssh-key ./$1 root@$2:/tmp ; echo model $1 copied to target is run ; ssh  -i target-ssh-key root@$2 modprobe rpi_gpio_irc_module ; ssh  -i target-ssh-key root@$2 /tmp/$1 -tf inf -w ; sleep 2" &
+if [ -n "$2" ] ; then
+
+  if [ -e target-ssh-key ] ; then
+    SSH_LOCAL_KEY_OPT="-i target-ssh-key"
+  fi
+
+  xterm -e "scp $SSH_LOCAL_KEY_OPT ./$1 root@$2:/tmp ; echo model $1 copied to target is run ; ssh $SSH_LOCAL_KEY_OPT target-ssh-key root@$2 modprobe rpi_gpio_irc_module ; ssh $SSH_LOCAL_KEY_OPT target-ssh-key root@$2 /tmp/$1 -tf inf -w ; sleep 2" &
+
+else
+
+  xterm -e "./$1 -tf inf -w ; sleep 2" &
+
+fi
